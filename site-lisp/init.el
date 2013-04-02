c117bb89c6c8bef7cea2315b7a8a5beae63d3e30
Heather <Heather@cynede.net>
2013-04-02 09:14:39 +0400

    settings load correction

--- site-lisp/init.el
+++ site-lisp/init.el
@@ -28,6 +28,33 @@
 (setq 
       init-settings-path (expand-file-name "settings" init-path)
       init-persistence-path (expand-file-name "persistence" init-path))
+(defun find-subdirs-containing (dir pattern)
+  (let* ((ret nil)
+                (files (directory-files dir))
+                (max-lisp-eval-depth 3000))
+    (while files
+      (let* ((file (car files))
+                    (path (expand-file-name file dir)))
+               (if (and (file-directory-p path)
+                               (not (string-match "^\\.+" file)))
+                   (setq ret (append ret (find-subdirs-containing path pattern)))
+                 (if (string-match pattern file)
+                     (add-to-list 'ret dir))))
+      (setq files (cdr files))) ret))
+(defun add-init-path-to-load-path ()
+  (interactive)
+  (setq load-path (append (find-subdirs-containing init-settings-path "\\.el$")
+                          (find-subdirs-containing init-packages-path "\\.el$")
+                          (list init-path)
+                          initial-load-path)))
+(dolist (file (directory-files init-settings-path t "-settings\\.el$"))
+    (condition-case e
+        (load-file file)
+        (error (warn "Error while loading settings file %s: %S" file (cdr e)))))
+(provide 'init)
+;; Add the init-path tree to the load path
+(setq initial-load-path load-path)
+(add-init-path-to-load-path)
 ;; C Formatting
 (setq-default c-basic-offset 4)
 (c-set-offset 'substatement-open '+)
