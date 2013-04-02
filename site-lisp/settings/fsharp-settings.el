112838a551438a37b79584d08ccde3b1c4fcc1fd
Heather <Heather@cynede.net>
2013-04-02 09:58:51 +0400

    additional settings for fsharp mode

--- site-lisp/settings/fsharp-settings.el
+++ site-lisp/settings/fsharp-settings.el
@@ -1,2 +1,10 @@
 (autoload 'fsharp-mode "fsharp-mode" "Major mode for editing F# code." t)
 (add-to-list 'auto-mode-alist '("\\.fs[iylx]?$" . fsharp-mode))
+
+(setq inferior-fsharp-program "/usr/bin/fsharpi --readline-")
+(setq fsharp-compiler "/usr/bin/fsharpc")
+
+(add-hook 'fsharp-mode-hook
+ (lambda ()
+   (define-key fsharp-mode-map (kbd "M-RET") 'fsharp-eval-region)
+   (define-key fsharp-mode-map (kbd "C-SPC") 'completion-at-point)))
