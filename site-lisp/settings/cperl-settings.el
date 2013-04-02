84375b70d157dae38c9470d170a486ebc2bbde08
Heather <Heather@cynede.net>
2013-04-02 10:11:25 +0400

    ++

--- site-lisp/settings/cperl-settings.el
+++ site-lisp/settings/cperl-settings.el
@@ -1,14 +1,9 @@
 (require 'cperl-mode)
-(setq 	auto-mode-alist (cons '("\\.p6\\'" . cperl-mode) auto-mode-alist)
-    auto-mode-alist (cons '("\\.t\\'" .  cperl-mode) auto-mode-alist)
-    auto-mode-alist (cons '("\\.pl\\'" .  cperl-mode) auto-mode-alist)
-    auto-mode-alist (cons '("\\.pm\\'" . cperl-mode) auto-mode-alist))
-
-(add-to-list 'interpreter-mode-alist
-             '("perl6" . cperl-mode))
-
+(setq auto-mode-alist (cons '("\\.p6\\'" . cperl-mode) auto-mode-alist)
+      auto-mode-alist (cons '("\\.t\\'"  . cperl-mode) auto-mode-alist)
+      auto-mode-alist (cons '("\\.pl\\'" . cperl-mode) auto-mode-alist)
+      auto-mode-alist (cons '("\\.pm\\'" . cperl-mode) auto-mode-alist))
+(add-to-list 'interpreter-mode-alist  '("perl6" . cperl-mode))
 ; indentation
 (setq cperl-indent-level 4)
-
-; my lovely closing parents style
 (setq cperl-indent-parens-as-block t)
