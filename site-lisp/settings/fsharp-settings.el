5f5d76cedb891b8d3380c25c1e0c7f5f82b2c7ac
Heather <Heather@cynede.net>
2013-04-02 09:15:29 +0400

    don't need to add default site

--- site-lisp/settings/fsharp-settings.el
+++ site-lisp/settings/fsharp-settings.el
@@ -1,3 +1,2 @@
-(add-to-list 'load-path "/usr/share/emacs/site-lisp/fsharpbinding")
 (autoload 'fsharp-mode "fsharp-mode" "Major mode for editing F# code." t)
 (add-to-list 'auto-mode-alist '("\\.fs[iylx]?$" . fsharp-mode))
