4349a0b23acd02a1f5c2ef98b0fbb1c7e843f8b6
Heather <heather@live.ru>
2014-02-28 10:30:46 +0400

    idris mode

---
+++ site-lisp/settings/idris-settings.el
@@ -0,0 +1,8 @@
+;;  Add Packages to load path
+(add-to-list 'load-path "~/.emacs.d/site-lisp/packages/idris-mode/")
+
+(autoload 'idris-mode "idris-mode.el"
+   "Major mode for editing idris programs." t)
+ (setq auto-mode-alist (cons '("\\.idr$" . 'idris-mode) auto-mode-alist))
+
+(idris-mode)
