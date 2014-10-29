b9b60b7d59abd15d9e93d640a77949f07d27ad2a
Heather <heather@live.ru>
2014-10-29 09:35:37 +0400

    remove agda mode...

--- site-lisp/settings/agda-settings.el
+++
@@ -1,7 +0,0 @@
-(load-file (let ((coding-system-for-read 'utf-8))
-                (shell-command-to-string "agda-mode locate")))
-
-(setq agda2-include-dirs
-      (list "." (expand-file-name "D:\\Heather\\Contrib\\P\\agda-stdlib\\src\\")))
-
-(require 'agda2)
