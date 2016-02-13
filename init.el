9ae9f7d6a2f208c971fd8c00b405f232d0f7111f
Heather <Heather@live.ru>
2016-02-13 13:06:04 +0400

    modified:   init.el

--- init.el
+++ init.el
@@ -20,3 +20,36 @@
         (server-edit)
         (make-frame-invisible nil t))
     (global-set-key (kbd "C-x C-c") 'hide-form))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(c-basic-offset 2)
+ '(case-fold-search nil)
+ '(cua-mode t nil (cua-base))
+ '(cua-selection-mode t)
+ '(current-language-environment "UTF-8")
+ '(cursor-type (quote bar))
+ '(global-hl-line-mode f)
+ '(global-linum-mode 0)
+ '(haskell-mode-hook (quote (turn-on-haskell-indentation)))
+ '(indent-tabs-mode nil)
+ '(inhibit-startup-screen t)
+ '(initial-buffer-choice nil)
+ '(lisp-body-indent 2)
+ '(lisp-indent-offset 2)
+ '(purescript-mode-hook (quote (turn-on-purescript-indent)))
+ '(setq backup-directory-alist t)
+ '(show-paren-mode t nil (paren))
+ '(show-trailing-whitespace t)
+ '(tab-width 2)
+ '(text-mode-hook (quote (turn-on-auto-fill text-mode-hook-identify)))
+ '(truncate-lines t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
