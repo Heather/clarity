cd5b5252f8607cd68ee74e43b34114bd703a1b2d
Heather <Heather@live.ru>
2016-05-18 13:16:23 +0400

    add prop-menu needed for idris-mode

--- site-lisp/settings/idris-settings.el
+++ site-lisp/settings/idris-settings.el
@@ -1,10 +1,13 @@
 ;;  Add Packages to load path
+(add-to-list 'load-path "~/.emacs.d/site-lisp/packages/prop-menu/")
 (add-to-list 'load-path "~/.emacs.d/site-lisp/packages/idris-mode/")
 
+(autoload 'prop-menu "prop-menu.el" "Popup menus" t)
 (autoload 'idris-mode "idris-mode.el"
    "Major mode for editing idris programs." t)
  (setq auto-mode-alist (cons '("\\.idr$" . 'idris-mode) auto-mode-alist))
 
+(require 'prop-menu)
 (require 'idris-mode)
 
 (add-to-list 'completion-ignored-extensions ".ibc")
