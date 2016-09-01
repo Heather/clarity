6ee64bb15f9ac995866376137254331e2292f779
Mikhail Pukhlikov <cynede@gentoo.org>
2016-09-01 11:56:23 +0000

    fix elpa thing, add rust-mode, haskell-mode

--- init.el
+++ init.el
@@ -2,11 +2,9 @@
 ; yet another emacs config
 
 ;; Melpa
-;(add-to-list 'package-archives
-;             '("melpa" . "https://melpa.org/packages/"))
-;(when (< emacs-major-version 24)
-;; For important compatibility libraries like cl-lib
-;  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
 ;; Deep Customization
